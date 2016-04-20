75ac2bd5a86a36f8be3bf89d463fc2b87239942f
Slezak Attila <attila.slezak@gmail.com>
2016-04-20 01:09:20 +0100

    LinkedListTask added but not ready yet, and some unnecessary files deleted

---
+++ LinkedList/LinkedListTask.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListTask
+{
+    class LinkedListTask
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> intLinkedList = new LinkedList<int>();
+            LinkedList<string> strLinkedList = new LinkedList<string>();
+            LinkedList<bool> linkedList = new LinkedList<bool>();
+        }
+    }
+}
