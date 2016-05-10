8e94aaa45b044eebaf2b6fefc616b9175da141b8
Slezak Attila <attila.slezak@gmail.com>
2016-05-10 23:30:00 +0100

    NumberPart and NumberPartPosComparer classes have been added

---
+++ Dictionary2/NumberPart.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary2
+{
+    class NumberPart
+    {
+        private bool _beginning = false;
+        private bool _ending = false;
+        private int _position;
+        private int _strLong;
+        private int _numLong;
+        private string _value;
+
+        public bool Beginning { get { return _beginning; } set { _beginning = value; } }
+        public bool Ending { get { return _ending; } set { _ending = value; } }
+        public int Position { get { return _position; } set { _position = value; } }
+        public int StrLong { get { return _strLong; } set { _strLong = value; } }
+        public int NumLong { get { return _numLong; } set { _numLong = value; } }
+        public string Value { get { return _value; } set { _value = value; } }
+
+        public NumberPart(int position, int strLong, int numLong, string value)
+        {
+            _position = position;
+            _strLong = strLong;
+            _numLong = numLong;
+            _value = value;
+        }
+    }
+}
---
+++ Dictionary2/NumberPartPosComparer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary2
+{
+    class NumberPartPosComparer : IComparer<NumberPart>
+    {
+        public int Compare(NumberPart first, NumberPart second)
+        {
+            if (first.Position > second.Position)
+                return 1;
+            else
+                return -1;
+        }
+    }
+}
